1397cc013c14b325072294ee29f8eca437a3888a
Adityapisal <117593124+Adityapisal@users.noreply.github.com>
2025-06-30 23:58:49 +0530

    Update Program.cs

--- IMS/Program.cs
+++ IMS/Program.cs
@@ -28,6 +28,13 @@ OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonComm
 
 var app = builder.Build();
 
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<InventoryManagementSystem.Data.ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
